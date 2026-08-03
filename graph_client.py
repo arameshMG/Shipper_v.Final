240e5ab92d1e41ee543ec9366b62688a8cdf207c
Aaditya Ramesh <aaditya.ramesh@medicalguardian.com>
2026-08-03 09:21:17 -0400

    Remove debug logging - production ready

--- graph_client.py
+++ graph_client.py
@@ -11,18 +11,9 @@ def get_graph_token():
         "grant_type": "client_credentials"
     }
 
-    print("TENANT ID:", config.GRAPH_TENANT_ID)
-    print("CLIENT ID:", config.GRAPH_CLIENT_ID)
-    print("CLIENT SECRET:", config. GRAPH_CLIENT_SECRET)
-
     response = requests.post(url, data=data, verify=False)
-
-    print("STATUS CODE:", response.status_code)
-    print("RAW RESPONSE:", response.text)
-
     token_data = response.json()
     return token_data["access_token"]
-
 
 def get_user_address(user_email, token):
     url = f"https://graph.microsoft.com/v1.0/users/{user_email}?$select=displayName, streetAddress"
